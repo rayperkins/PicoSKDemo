6d8a1eb8b77b1fdff95e2928963a3aa9e0e6bdb2
Ray Perkins <raymond@perkins.org.nz>
2023-05-12 14:56:17 +1200

    initial commit

---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Runtime;
+using System;
+
+namespace PicoSKDemo;
+
+[Application]
+public class MainApplication : Application
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+}
