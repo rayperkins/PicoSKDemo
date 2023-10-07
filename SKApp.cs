477da1a679d440c80e764716054279abc247bdf5
Ray Perkins <ray.perkins@weareglobal.co.nz>
2023-10-08 10:55:13 +1300

    add lighting demo

--- SKApp.cs
+++ SKApp.cs
@@ -1,9 +1,11 @@
 ﻿using Android.Icu.Number;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PicoSKDemo.Scenes;
 using PicoSKDemo.Services.Abstractions;
 using StereoKit;
 using System;
+using System.Threading.Tasks;
 
 namespace PicoSKDemo;
 
@@ -22,10 +24,19 @@ public class SKApp
 	Pose  cubePose = new Pose(0, 0, -0.5f, Quat.Identity);
 	Model cube;
 	Matrix   floorTransform = Matrix.TS(new Vec3(0, -1.5f, 0), new Vec3(30, 0.1f, 30));
-	Material floorMaterial;
     Solid floorSolid;
     PassthroughFBExt passthrough;
 	Model trainModel;// = Model.FromFile("Assets/train.glb");
+
+    private StaticScene staticScene;
+    private BakedScene bakedScene;
+    private float bakedSamples = 16;
+    private Pose bakeSettingsPose = new Pose(0, 0, -0.5f, Quat.LookDir(0, 0, 1));
+
+    private Mesh floorMesh;
+    private Material floorMaterial;
+    private Mesh cubeMesh;
+    private Material cubeMaterial;
 
 
 
@@ -42,14 +53,30 @@ public class SKApp
 
     public void Init()
 	{
-		// Create assets used by the app
-		cube = Model.FromMesh(
-			Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
-			Default.MaterialUI);
-        trainModel = Model.FromFile("pallet_conveyor_model_3.glb");
-        floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
-        floorMaterial.Transparency = Transparency.Blend;
-        floorSolid = new Solid(World.HasBounds ? World.BoundsPose.position : new Vec3(0, -1.5f, 0), Quat.Identity, SolidType.Immovable);
+        // Create assets used by the app
+        //cube = Model.FromMesh(
+        //	Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
+        //	Default.MaterialUI);
+        //      trainModel = Model.FromFile("pallet_conveyor_model_3.glb");
+        //      floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
+        //      floorMaterial.Transparency = Transparency.Blend;
+        //      floorSolid = new Solid(World.HasBounds ? World.BoundsPose.position : new Vec3(0, -1.5f, 0), Quat.Identity, SolidType.Immovable);
+
+        floorMesh = Mesh.GeneratePlane(V.XY(10, 10), 20);
+        cubeMesh = Mesh.GenerateCube(Vec3.One, 8);
+        floorMaterial = Material.Default.Copy();
+        floorMaterial[MatParamName.DiffuseTex] = Tex.FromFile("test.png");
+        cubeMaterial = Material.Default.Copy();
+        cubeMaterial[MatParamName.DiffuseTex] = Tex.FromFile("floor.png");
+
+        staticScene = GenerateScene();
+
+        bakedScene = new BakedScene();
+        bakedScene.AddDirectionalLight(V.XYZ(-1, -1, -1), Color.White, 1);
+        bakedScene.SetSky(Renderer.SkyLight);
+        bakedScene.Bake(staticScene, 0);
+        Task.Run(() => bakedScene.Bake(staticScene, (int)bakedSamples));
+
 
         if (!SK.System.worldOcclusionPresent)
 		{
@@ -64,30 +91,80 @@ public class SKApp
         //_cameraService.InitCamera();
     }
 
-	public void Step()
-	{
-		if (SK.System.displayType == Display.Opaque)
-			Default.MeshCube.Draw(floorMaterial, floorTransform);
-
-		UI.Handle("Cube", ref cubePose, cube.Bounds);
-        cube.Draw(cubePose.ToMatrix());
-
-        trainModel.Draw(Matrix.TRS(new Vec3(-37.0f, -2.0f, 6.0f), Quat.FromAngles(0, 0, 0), 1.0f));
-
+    private StaticScene GenerateScene()
+    {
+        float height = -1.6f;
         if (World.HasBounds)
-        {
-            Vec2 s = World.BoundsSize / 2;
-            Matrix pose = World.BoundsPose.ToMatrix();
-            Vec3 tl = pose.Transform(new Vec3(s.x, 0, s.y));
-            Vec3 br = pose.Transform(new Vec3(-s.x, 0, -s.y));
-            Vec3 tr = pose.Transform(new Vec3(-s.x, 0, s.y));
-            Vec3 bl = pose.Transform(new Vec3(s.x, 0, -s.y));
+            height = World.BoundsPose.position.y;
 
-            Lines.Add(tl, tr, Color.White, 1.5f * U.cm);
-            Lines.Add(bl, br, Color.White, 1.5f * U.cm);
-            Lines.Add(tl, bl, Color.White, 1.5f * U.cm);
-            Lines.Add(tr, br, Color.White, 1.5f * U.cm);
+        Random r = new Random();
+        StaticScene result = new StaticScene();
+        result.AddMesh(floorMesh, floorMaterial, Matrix.T(0, height, 0));
+        for (int i = 0; i < 20; i++)
+        {
+            Vec3 at = new Vec3(r.NextSingle() * 10 - 5, r.NextSingle() * 4 + height, r.NextSingle() * 10 - 5);
+            Vec3 scale = new Vec3(r.NextSingle() * 3 + 0.5f, r.NextSingle() * 3 + 0.5f, r.NextSingle() * 3 + 0.5f);
+            result.AddMesh(cubeMesh, cubeMaterial, Matrix.TS(
+                at + V.XYZ(0, scale.y / 2.0f, 0), scale));
         }
+
+        return result;
+    }
+
+    public void Step()
+	{
+        bakedScene.Draw();
+        DrawWindowBakeSettings();
+
+        //if (SK.System.displayType == Display.Opaque)
+        //	Default.MeshCube.Draw(floorMaterial, floorTransform);
+
+        //UI.Handle("Cube", ref cubePose, cube.Bounds);
+        //      cube.Draw(cubePose.ToMatrix());
+
+        //      //trainModel.Draw(Matrix.TRS(new Vec3(-37.0f, -2.0f, 6.0f), Quat.FromAngles(0, 0, 0), 1.0f));
+
+        //      if (World.HasBounds)
+        //      {
+        //          Vec2 s = World.BoundsSize / 2;
+        //          Matrix pose = World.BoundsPose.ToMatrix();
+        //          Vec3 tl = pose.Transform(new Vec3(s.x, 0, s.y));
+        //          Vec3 br = pose.Transform(new Vec3(-s.x, 0, -s.y));
+        //          Vec3 tr = pose.Transform(new Vec3(-s.x, 0, s.y));
+        //          Vec3 bl = pose.Transform(new Vec3(s.x, 0, -s.y));
+
+        //          Lines.Add(tl, tr, Color.White, 1.5f * U.cm);
+        //          Lines.Add(bl, br, Color.White, 1.5f * U.cm);
+        //          Lines.Add(tl, bl, Color.White, 1.5f * U.cm);
+        //          Lines.Add(tr, br, Color.White, 1.5f * U.cm);
+        //      }
+    }
+
+    
+    private void DrawWindowBakeSettings()
+    {
+        UI.WindowBegin("Bake Settings", ref bakeSettingsPose);
+        UI.Label("Samples"); UI.SameLine(); UI.HSlider("Samples", ref bakedSamples, 0, 512, 1);
+
+        UI.PushEnabled(!bakedScene.Baking);
+        if (UI.Button("Regenerate"))
+        {
+            staticScene = GenerateScene();
+            bakedScene.Bake(staticScene, 0);
+        }
+
+        UI.SameLine();
+        if (UI.Button("Bake"))
+            Task.Run(() => bakedScene.Bake(staticScene, (int)bakedSamples));
+
+        UI.PopEnabled();
+        if (bakedScene.Baking)
+        {
+            UI.SameLine();
+            UI.Label("Baking...");
+            UI.ProgressBar(bakedScene.BakingProgress);
+        }
+        UI.WindowEnd();
     }
 
 
---
+++ Scenes/StaticScene.cs
@@ -0,0 +1,95 @@
+﻿using StereoKit;
+using System.Collections.Generic;
+
+namespace PicoSKDemo.Scenes
+{
+	public class StaticScene
+	{
+		internal List<StaticSceneItem> _items = new List<StaticSceneItem>();
+
+		public void AddModel(Model model, Matrix at)
+		{
+			if (model == null) return;
+
+			foreach (ModelNode node in model.Nodes)
+			{
+				if (node.Mesh == null) continue;
+				
+				string name = node.Name;
+
+				bool visible = 
+					name.Contains("[invisible]") == false ||
+					(node.Info.Contains("visible") && node.Info["visible"] == "true");
+				bool solid = 
+					name.Contains("[intangible]") == false ||
+					(node.Info.Contains("solid") && node.Info["solid"] == "true");
+
+				AddMesh(node.Mesh, node.Material, node.ModelTransform * at, visible, solid);
+			}
+		}
+
+		public void AddMesh(Mesh mesh, Material material, Matrix at, bool solid = true, bool visible = true)
+		{
+			StaticSceneItem item = new StaticSceneItem();
+			item.visible      = visible;
+			item.solid        = solid;
+			item.material     = material;
+			item.mesh         = mesh;
+			item.transform    = at;
+			item.invTransform = at.Inverse;
+			_items.Add(item);
+		}
+
+		public bool Raycast(Ray worldRay, out Ray at)
+		{
+			float closest = float.MaxValue;
+			at = default;
+			for (int i = 0; i < _items.Count; i++)
+			{
+				if (_items[i].Raycast(worldRay, out Ray curr))
+				{
+					float dist = Vec3.DistanceSq(curr.position, worldRay.position);
+					if (dist < closest)
+					{
+						closest = dist;
+						at      = curr;
+					}
+				}
+			}
+			return closest != float.MaxValue;
+		}
+
+		public void Draw()
+		{
+			for (int i = 0; i < _items.Count; i++)
+				_items[i].Draw();
+		}
+	}
+
+	struct StaticSceneItem
+	{
+		internal Matrix   transform;
+		internal Matrix   invTransform;
+		internal Mesh     mesh;
+		internal Material material;
+		internal bool     solid;
+		internal bool     visible;
+
+		public bool Raycast(Ray worldRay, out Ray intersection)
+		{
+			if (solid)
+			{
+				bool result = mesh.Intersect(invTransform.Transform(worldRay), out intersection);
+				if (result) intersection = transform.Transform(intersection);
+				return result;
+			}
+			else
+			{
+				intersection = default;
+				return false;
+			}
+		}
+
+		public void Draw() { if (visible) mesh.Draw(material, transform); }
+	}
+}
